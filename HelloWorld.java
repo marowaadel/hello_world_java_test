477b9b25ccccebb7b044346ea78b1cf1031c81ff
marwa <47919176+marowaadel@users.noreply.github.com>
2022-09-23 01:58:55 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld{
+    public static void main(String[] args)
+     {
+		 //if (args.length > 0) {
+            //System.out.println("The command line arguments are:");
+ 
+            // iterating the args array and printing
+            // the command line arguments
+/*
+            for (String val : args)
+                System.out.print(val+" ");
+        }
+        else
+            System.out.println("No command line "
+                               + "arguments found.");
+*/
+        System.out.println("hello world 1");
+    }
+
+     }
