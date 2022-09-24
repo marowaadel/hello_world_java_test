2d8c4b25ec4e703acd3b2cc70b47d0dc4eecd7d0
marwa <47919176+marowaadel@users.noreply.github.com>
2022-09-25 01:26:10 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld{
     public static void main(String[] args)
      {
-        System.out.println("hello world 1");
-	System.out.println("hello world 2");
-	System.out.println("hello world 3");
+        System.out.println("hello world 1 hhhhhh");
+	System.out.println("hello world 2 !!!!");
+	System.out.println("hello world 3 lap lap lap");
 	    System.out.println("hello world there");
 	    System.out.println("hello world again");
     }
